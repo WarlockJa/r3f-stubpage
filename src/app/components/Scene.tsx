7bf13563556654ac19c0b253979d668c61932b81
warlockja <warlockja@gmail.com>
2024-02-22 19:37:52 +0300

    fix deployment

--- src/app/components/Scene.tsx
+++ src/app/components/Scene.tsx
@@ -3,15 +3,17 @@ import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
 import Text404 from "./Text404";
 import { Environment } from "@react-three/drei";
+import { Glassshards } from "./Glassshards";
 
 export default function Scene() {
   return (
-    <Canvas orthographic camera={{ position: [0, 0, 1], zoom: 800 }}>
+    <Canvas orthographic camera={{ position: [0, 0, 1], zoom: 1500 }}>
       {/* <OrbitControls /> */}
       <directionalLight intensity={3} position={[0, 0.1, 1]} />
       {/* <Model /> */}
+      <Glassshards />
       <Text404 />
-      {/* <Environment preset="city" /> */}
+      <Environment preset="city" />
     </Canvas>
   );
 }
